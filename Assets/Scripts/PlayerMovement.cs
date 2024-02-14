58d4f6fcdba16253c23e5d33965c19981babd172
Ruslan Issabekov <bad.rak202@gmail.com>
2024-02-15 01:32:09 +0600

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+
+namespace Scripts.PlayerMovement
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] public Rigidbody2D rb;
+        [SerializeField] public Animator anim;
+
+        [Header("Attribute")]
+        [SerializeField] public float moveSpeed;
+        [SerializeField] public float jumpForce;
+
+        private float moveDirection;
+        private bool facingRight = true;
+        private bool isJumping = false;
+        private bool isGround = false;
+
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            Inputs();
+            DirectionControl();
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Inputs()
+        {
+            moveDirection = Input.GetAxis("Horizontal");
+            if (moveDirection != 0)
+            {
+                anim.SetBool("isWalk", true);
+            }
+            else
+            {
+                anim.SetBool("isWalk", false);
+            }
+
+            if (Input.GetButtonDown("Jump") && isGround)
+            {
+                isJumping = true;
+            }
+        }
+
+        private void Move()
+        {
+            rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+
+            if (isJumping)
+            {
+                rb.AddForce(new Vector2(0f, jumpForce * 100f));
+                isJumping = false;
+                isGround = false;
+            }
+        }
+
+        private void DirectionControl()
+        {
+            if (moveDirection > 0 && !facingRight)
+            {
+                FlipCharacter();
+            }
+            else if (moveDirection < 0 && facingRight)
+            {
+                FlipCharacter();
+            }
+        }
+
+        private void FlipCharacter()
+        {
+            facingRight = !facingRight;
+            transform.Rotate(0f, 180f, 0f);
+        }
+
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Ground") || other.gameObject.CompareTag("Platform"))
+            {
+                isGround = true;
+            }
+        }
+    }
+}
